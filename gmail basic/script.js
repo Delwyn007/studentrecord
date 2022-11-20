063ad63fa6695a989ecc871e6dfc6c938183f071
Delwyn007 <91654573+Delwyn007@users.noreply.github.com>
2022-11-20 15:21:15 +0530

    Add files via upload

--- gmail basic/script.js
+++ gmail basic/script.js
@@ -1,45 +1,59 @@
-
 function openForm() {
-    if(document.getElementById("myForm").style.display === "none"){
-      document.getElementById("myForm").style.display = "block";
-    }
-    else{
-      document.getElementById("myForm").style.display = "none";
-    }
-  }
-
-  
-
-function hideMyForm(){
+  if (document.getElementById("myForm").style.display === "none") {
+    document.getElementById("myForm").style.display = "block";
+  } else {
     document.getElementById("myForm").style.display = "none";
   }
-
-  function val(){
-    if(b1==""){
-      if(b2==""){
-        alert("please enter a valid subject and a message "+b1);
-      }
-    }
-    else{
-      alert("your subject is entered ");
-    }
-    var b1=document.getElementById("a").value;
-    var b2=document.getElementById("b").value;
-    console.log(b1);
-    const mail = {
-      sub: b1,
-      comp: b2,
-      read:true,
-    }
-   window.localStorage.setItem('user', JSON.stringify(mail));
-  }  
-  var bc=document.getElementById("sur");
-  bc.onclick=function(){
-    val()
-  }
-  function vals(){
-    while(b1!==null && b2!==null){
-     
-  }
 }
-  
+window.onload = function hh() {
+  debugger;
+};
+
+function hideMyForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+function val() {
+  if (a == "") {
+    if (b == "") {
+      alert("please enter a valid subject and a message " + a);
+    }
+  } else {
+    alert("your subject is entered ");
+  }
+  var a = document.getElementById("a").value;
+  var b = document.getElementById("b").value;
+  console.log(a);
+  var user = JSON.parse(sessionStorage.getItem("user") || "[]");
+  var mail = {
+    sub: a,
+    comp: b,
+    read: false,
+  };
+  user.push(mail);
+  sessionStorage.setItem("user", JSON.stringify(user));
+}
+
+var bc = document.getElementById("val");
+bc.onclick = function () {
+  val();
+};
+function vals() {
+  while (b1 !== null && b2 !== null) {}
+}
+
+/*
+user.push(Message)
+var div1 = document.createElement("div");
+var sub = document.createElement("p");
+var com = document.createElement("p");
+var div2 = document.createElement("div");
+
+
+
+sub.innerHTML = "Subject: " + sub;
+comp.innerHTML = "Compose: " + comp;
+
+userdiv.append()
+
+*/
